04974743914ade0906f0341d711272f9d31c9f82
OlegBvG <borisov_og@mail.ru>
2019-07-20 00:34:43 +0300

    Copy Directory with AbsolutePath

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String sourceDirStr = "..\\CopyDirectory";
-        String targetDirStr = "d:\\ArhCopyDirectory";
+        String sourceDirStr = "d:\\1\\1";
+        String targetDirStr = "d:\\1\\2";
 
         try {
             copyDir(sourceDirStr,  targetDirStr);
@@ -20,11 +20,25 @@ public class Main {
             Path sourceDir = Paths.get(source);
             Path targetDir = Paths.get(target).resolve((sourceDir).getFileName());
 
+            System.out.println("Копируется директория: " + sourceDir + " в директорию: " + target);
+
+            if (Files.notExists(targetDir)) Files.createDirectories(targetDir);
+
             Files.walk(sourceDir).forEach(p ->
             {
                 try {
-                  Path q = targetDir.resolve(p).normalize();
-                  Files.copy(p, q, StandardCopyOption.REPLACE_EXISTING);
+                  Path q = targetDir.resolve(sourceDir.relativize(p).normalize());
+
+                    if (Files.isDirectory(p)) {
+                        if (Files.notExists(q)){
+                            Files.createDirectories(q);
+                            System.out.println("Создана папка: " + p +  " => " + q);
+                        }
+                    }
+                    else {
+                        Files.copy(p, q, StandardCopyOption.REPLACE_EXISTING);
+                        System.out.println("Скопирован файл: " + p +  " => " + q);
+                    }
                 } catch (IOException ex) {
                     System.out.println("Ошибка чтения/записи");
                 }
